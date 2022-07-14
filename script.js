85a320247016404236367a5c62b60f5abfecdfe2
Alex Pillion <apillion@gmail.com>
2022-07-13 21:03:00 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('Javascript is loaded...');
+
+function loading() {
+    alert('Loading weather report...');
+}
+
+let takeaway = document.querySelector(".cookie-policy");
+
+function acceptCookies() {
+    takeaway.remove();
+}
+
+//I can't for the life of me figure out how to convert the temperatures to F and C!//
